95e5a03daf74f37fee816435286d28823a329e80
Joe Kutner <jpkutner@gmail.com>
2017-06-27 12:58:37 -0500

    Converted to a recompose install only buildpack

--- opt/proxy.rb
+++
@@ -1,9 +0,0 @@
-require 'webrick'
-require 'webrick/httpproxy'
-
-proxy = WEBrick::HTTPProxyServer.new Port: ENV['PORT'].to_i
-
-trap 'INT'  do proxy.shutdown end
-trap 'TERM' do proxy.shutdown end
-
-proxy.start
